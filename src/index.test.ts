d8dc1c37fc22c485279c26d8280485281463412d
clement <clement.ribourt@ynov.com>
2020-03-06 16:06:57 +0100

    Leap Year function

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,4 +6,7 @@ describe("Leap Year tests", function() {
   it("should return true if divisible by 400", function() {
     expect(LeapYear(800)).to.equal(true);
   });
+  it("should return true if divisible by 4", function() {
+    expect(LeapYear(800)).to.equal(true);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 export function LeapYear(year: number) {
-    if(year % 400 === 0){
-        return true
-    }
+  let i = 0;
+  while (i <= year) {
+    if (i % 4 === 0) {
+      if (i % 100 === 0) {
+        if (i % 400 === 0) {
+          console.log(i, " is a Leap Year");
+          i++;
+        } else i++;
+      } else i++;
+    } else i++;
+  }
 }
+
+console.log(LeapYear(2020));
